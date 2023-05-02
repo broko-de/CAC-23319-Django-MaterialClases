bf71493bb06de34812a03f23bd17a0672e9f483b
Fede Liquin <josefliquin@gmail.com>
2023-05-02 11:56:11 -0300

    REUNION 19: Se agrega modificacion al proyecto para aceptar conexión con PostgreSQL

--- proyecto_clase/proyecto_23319/proyecto_23319/settings.py
+++ proyecto_clase/proyecto_23319/proyecto_23319/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'proyecto_23319.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': env("DATABASE_HOST"),
+        'PORT': env("DATABASE_PORT"),
     }
 }
 
