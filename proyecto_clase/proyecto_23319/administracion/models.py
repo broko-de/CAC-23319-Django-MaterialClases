0cb5c4dd0a96716273c042457163cefddbb6155d
Fede Liquin <josefliquin@gmail.com>
2023-05-11 12:04:55 -0300

    REUNION 22: Contenido visto en vivo

--- proyecto_clase/proyecto_23319/administracion/models.py
+++ proyecto_clase/proyecto_23319/administracion/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 #Modelo UNICO - SOLUCION 1
-# class Persona(models.Model):
+# class PersonaU(models.Model):
 #     nombre = models.CharField(max_length=100,verbose_name='Nombre')
 #     apellido = models.CharField(max_length=150,verbose_name='Apellido')
 #     email = models.EmailField(max_length=150,null=True)
@@ -23,37 +25,39 @@ from django.db import models
 
 # class EstudianteAbs(PersonaAbs):
 #     matricula = models.CharField(max_length=10,verbose_name='Matricula')
+#     baja = models.BooleanField(default=0,null=True)
 
 # class InstructorAbs(PersonaAbs):
 #     legajo = models.CharField(max_length=10,verbose_name='Legajo')
 
 #HERENCIA - SOLUCION 3
-# class Persona(models.Model):
-#     nombre = models.CharField(max_length=100,verbose_name='Nombre')
-#     apellido = models.CharField(max_length=150,verbose_name='Apellido')
-#     email = models.EmailField(max_length=150,null=True)
-#     dni = models.IntegerField(verbose_name="DNI")
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100,verbose_name='Nombre')
+    apellido = models.CharField(max_length=150,verbose_name='Apellido')
+    email = models.EmailField(max_length=150,null=True)
+    dni = models.IntegerField(verbose_name="DNI")
 
-# class Estudiante(Persona):
-#     matricula = models.CharField(max_length=10,verbose_name='Matricula')
-#     baja = models.BooleanField(default=0)
+class Estudiante(Persona):
+    matricula = models.CharField(max_length=10,verbose_name='Matricula')
+    baja = models.BooleanField(default=0)
 
-#     def __str__(self):
-#         return f"{self.matricula} - {self.nombre} {self.apellido}"
+    def __str__(self):
+        return f"{self.matricula} - {self.nombre} {self.apellido}"
     
-#     def soft_delete(self):
-#         self.baja=True
-#         super().save()
+    def soft_delete(self):
+        self.baja=True
+        super().save()
     
-#     def restore(self):
-#         self.baja=False
-#         super().save()
+    def restore(self):
+        self.baja=False
+        super().save()
     
-#     class Meta():
-#         verbose_name_plural = 'Estudiantes'
+    class Meta():
+        verbose_name_plural = 'Estudiantes'
+        # db_table = 'nombre_tabla'
 
-# class Instructor(Persona):
-#     legajo = models.CharField(max_length=10,verbose_name='Legajo')
+class Instructor(Persona):
+    legajo = models.CharField(max_length=10,verbose_name='Legajo')
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50,verbose_name='Nombre')
@@ -88,6 +92,38 @@ class Curso(models.Model):
 # cuenta el diagrama de clases.
 class Comision(models.Model):
     nombre = models.CharField(max_length=100,verbose_name="Nombre")
-    horario = models.CharField(verbose_name="Horario",null=True,default=None)
+    horario = models.CharField(max_length=100,verbose_name="Horario",null=True,default=None)
     link_meet = models.URLField(max_length=100,verbose_name='Link de Meet')
     curso = models.ForeignKey(Curso,on_delete=models.CASCADE) #relacion mucho a uno
+    estudiantes = models.ManyToManyField(Estudiante,through='Inscripcion')
+
+#Modelo que genera tabla intermedia automaticamente
+# class ComisionMTM(models.Model):
+#     nombre = models.CharField(max_length=100,verbose_name="Nombre")
+#     horario = models.CharField(max_length=100,verbose_name="Horario",null=True,default=None)
+#     link_meet = models.URLField(max_length=100,verbose_name='Link de Meet')
+#     curso = models.ForeignKey(Curso,on_delete=models.CASCADE) #relacion mucho a uno
+#     estudiantes = models.ManyToManyField(Estudiante)
+
+class Inscripcion(models.Model):
+    
+    ESTADOS = [
+        (1,'Inscripto'),
+        (2,'Cursando'),
+        (3,'Egresado'),
+    ]
+    fecha_creacion = models.DateField(auto_now_add=True,verbose_name='Fecha de creacion')
+    estudiante = models.ForeignKey(Estudiante,on_delete=models.CASCADE)
+    comision = models.ForeignKey(Comision,on_delete=models.CASCADE)
+    estado = models.IntegerField(choices=ESTADOS,default=1)
+
+    def __str__(self):
+        return self.estudiante.nombre
+    
+
+    class Perfil(models.Model):
+        """MODELO QUE PERMITE DEL USER MODEL DE DJANGO PARA AGREGERLE CAMPOS EXTRAS"""
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        telefono = models.CharField(max_length=20,verbose_name='Teléfono')
+        domicilio = models.CharField(max_length=20,verbose_name='Domicilio')
+        foto = models.ImageField(upload_to='perfiles/',null=True,verbose_name='Foto Perfil')
