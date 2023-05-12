267c000b69461521a7b594416f22a2942ebeb835
Fede Liquin <josefliquin@gmail.com>
2023-05-12 02:30:08 -0300

    REUNION 23: Se agrega CRUD del modelo categoria

--- proyecto_clase/proyecto_23319/administracion/urls.py
+++ proyecto_clase/proyecto_23319/administracion/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index_administracion,name='inicio_administracion'),
+    path('categorias/', views.categorias_index,name='categorias_index'),
+    path('categorias/nuevo/', views.categorias_nuevo,name='categorias_nuevo'),
+    path('categorias/editar/<int:id_categoria>', views.categorias_editar,name='categorias_editar'),
+    path('categorias/eliminar/<int:id_categoria>', views.categorias_eliminar,name='categorias_eliminar'),
+
 ]
--- proyecto_clase/proyecto_23319/administracion/views.py
+++ proyecto_clase/proyecto_23319/administracion/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from administracion.forms import CategoriaForm
+
+from administracion.models import Categoria
 
 # Create your views here.
 def index_administracion(request):
     variable = 'test variable'
     return render(request,'administracion/index_administracion.html',{'variable':variable})
 
+"""
+    CRUD Categorias
+"""
+def categorias_index(request):
+    #queryset
+    categorias = Categoria.objects.filter(baja=False)
+    return render(request,'administracion/categorias/index.html',{'categorias':categorias})
+
+def categorias_nuevo(request):
+    if(request.method=='POST'):
+        formulario = CategoriaForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('categorias_index')
+    else:
+        formulario = CategoriaForm()
+    return render(request,'administracion/categorias/nuevo.html',{'formulario':formulario})
+
+def categorias_editar(request,id_categoria):
+    try:
+        categoria = Categoria.objects.get(pk=id_categoria)
+    except Categoria.DoesNotExist:
+        return render(request,'administracion/404_admin.html')
+
+    if(request.method=='POST'):
+        formulario = CategoriaForm(request.POST,instance=categoria)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('categorias_index')
+    else:
+        formulario = CategoriaForm(instance=categoria)
+    return render(request,'administracion/categorias/editar.html',{'formulario':formulario})
+
+def categorias_eliminar(request,id_categoria):
+    try:
+        categoria = Categoria.objects.get(pk=id_categoria)
+    except Categoria.DoesNotExist:
+        return render(request,'administracion/404_admin.html')
+    categoria.soft_delete()
+    return redirect('categorias_index')
